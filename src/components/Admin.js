1db3e888b127d4e10795b72d3e232fb2121c704c
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2021-03-08 09:58:08 +0100

    Admin Log

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,7 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Admin = (props) => {
-    console.log( props.match.params);
+    console.log( props.history);
+    console.log( "//");
+    console.log( props.match);
+    console.log( "//");
+    console.log( props.location);
     return (
         <div>
             Bonjour Mr {props.match.params.ID}
--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
     return (
         <div>
-            Test Error
+            <strong>Utilisateur non-identifier</strong>
         </div>
     )
 }
