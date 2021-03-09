7970f8f3d0301be4ad3c651eca8d0ac0eb864bf3
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2021-03-09 17:40:38 +0000

    end day

--- src/components/DateFirebaseNow.js
+++ src/components/DateFirebaseNow.js
@@ -1,29 +1,28 @@
 import { React } from 'react'
 
 const DateFirebaseNow = () => {
+    //// Main timer
     const time = async () => {
         const url = "http://recrutement.notoriety-group.com/date.php"
         const response = await fetch(url)
         const data = await response.json()
         console.log(data);
         const date = new Date(data*1000)
-        console.log(date.toGMTString()+"====>Notoriety");
+        console.log(date.toGMTString());
     }
-
-    //// Back-up
+    // //// Back-up
     // const time = async () => {
     //     const url = "http://worldtimeapi.org/api/timezone/Europe/Paris.json"
     //     const response = await fetch(url)
     //     const data = await response.json()
-    //     console.log(data.unixtime);
+    //     console.log(data);
     //     const date = new Date(data.unixtime*1000)
-    //     console.log(date.toGMTString()+"====>Paris");
+    //     console.log(date.toGMTString());
     // }
-    
+
     return (
         <>
-        <button onClick={time}>Clic</button>
-
+            <button onClick={time}>Clic</button>
         </>
     )
 }
