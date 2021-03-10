17cb384ed3018acf0f15ef6cd3b6b3e9b23f7084
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2021-03-10 11:37:45 +0000

    js export to js

---
+++ src/Util/test.js
@@ -0,0 +1,12 @@
+
+export function double(n){
+    return n*2
+}
+
+export function triple(n){
+    return n*3
+}
+
+export function Quadra(n){
+    return n*4
+}
--- src/components/Admin/Hey.js
+++ src/components/Admin/Hey.js
@@ -1,4 +1,10 @@
-import {Test} from "./test"
+import {double,triple,Quadra} from "../../Util/test.js"
+import A from "../../Util/test2.js"
 
-let A = Test.A()
-console.log(A);
+/////////// test 1
+console.log(double(2));
+console.log(triple(2));
+console.log(Quadra(2));
+
+/////////// test 2
+console.log(A(2));
